0e1fe13ac72d7d5969c70048888cefc89a80b5e0
Lucas <72669100+CiceroLucas@users.noreply.github.com>
2024-06-03 10:41:40 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,16 @@ public class Main {
     public static void main(String[] args) {
         IPhone aparelho = new IPhone();
 
-        System.out.println("------------->");
         aparelho.selecionarMusica("Sepultura");
         aparelho.tocar();
         aparelho.pausar();
 
-        System.out.println("------------->");
         aparelho.atender();
         aparelho.iniciarCorreioVoz();
         aparelho.ligar(12345678);
 
-        System.out.println("------------->");
         aparelho.exibirPagina();
         aparelho.atualizarPagina();
         aparelho.adicionarNovaAba();
-
-        System.out.println("------------->");
     }
 }
